a361065aa1d41da363b74fca8b1ab547679b2ba1
moo-on <wns0159@gmail.com>
2021-09-17 23:45:01 +0900

    frontcontroller

---
+++ src/com/web/frontcontroller/MemberFrontController.java
@@ -0,0 +1,19 @@
+package com.web.frontcontroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberFrontController
+ */
+@WebServlet("*.do")
+public class MemberFrontController extends HttpServlet {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = request.getRequestURI();
+	}
+
+}
