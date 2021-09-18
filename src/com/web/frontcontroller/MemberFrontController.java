7912eabccd7955198a2b02086049948b8a1067db
moo-on <wns0159@gmail.com>
2021-09-18 23:54:10 +0900

    frontcontroller - url작업

--- src/com/web/frontcontroller/MemberFrontController.java
+++ src/com/web/frontcontroller/MemberFrontController.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.util.*;
 /**
  * Servlet implementation class MemberFrontController
  */
@@ -14,6 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberFrontController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String url = request.getRequestURI();
+		String ctx = request.getContextPath(); // ctx = jsp-tutorial
+		
+		String command = url.substring(ctx.length()); // URI= jsp-tutorial/memberInster.do, command = memberInsert.do
+		
+		//분기 작업
+		if(command.equals("/memberList.do")) {
+			
+		}
+		else if(command.equals("/memberList.do")) {
+			
+		}
 	}
 
 }
