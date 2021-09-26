b32359d3ba5ce30d8e4f668612a07b070b22d6e8
moo-on <wns0159@gmail.com>
2021-09-26 17:22:11 +0900

    ifelse branch -> handlerMapping

---
+++ src/com/web/frontcontroller/HandlerMapping.java
@@ -0,0 +1,29 @@
+package com.web.frontcontroller;
+
+import java.util.HashMap;
+
+import com.web.controller.Controller;
+import com.web.controller.MemberContentController;
+import com.web.controller.MemberDeleteController;
+import com.web.controller.MemberInsertController;
+import com.web.controller.MemberListController;
+import com.web.controller.MemberRegisterController;
+import com.web.controller.MemberUpdateController;
+
+public class HandlerMapping {
+	private HashMap<String, Controller> mappings;
+	
+	public HandlerMapping() {
+		mappings = new HashMap<>();
+		mappings.put("/memberList.do", new MemberListController());
+		mappings.put("/memberInsert.do", new MemberInsertController());
+		mappings.put("/memberUpdate.do", new MemberUpdateController());
+		mappings.put("/memberDelete.do", new MemberDeleteController());
+		mappings.put("/memberContent.do", new MemberContentController());
+		mappings.put("/memberRegister.do", new MemberRegisterController());
+	}
+	// Controller °´Ã¼ ¹ÝÈ¯
+	public Controller getController(String key) {
+		return mappings.get(key);
+	}
+}
